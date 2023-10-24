4bcb88390339384d2d28568daf1629e7d676eec8
UnitedPuggs <edpoulsonv@hotmail.com>
2023-10-23 23:57:56 -0700

    fix: mobile styling

--- src/routes/[link]/+page.svelte
+++ src/routes/[link]/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex flex-col gap-2 justify-center items-center mt-10 border-2 border-gray-400 w-fit mx-auto p-2 rounded-lg">
     {#each links.links as link}
-        <a href={link} target="_blank" class="rounded-lg bg-white text-black font-bold w-96 px-1 py-0.5 break-all border border-black hover:opacity-75">
+        <a href={link} target="_blank" class="rounded-lg bg-white text-black font-bold w-56 md:w-96 px-1 py-0.5 break-all border border-black hover:opacity-75">
             {link}
         </a>
     {/each}
