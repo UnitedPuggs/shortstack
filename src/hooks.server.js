ce4b89576b300f351b6b619ab664e4c1ad0d0166
UnitedPuggs <edpoulsonv@hotmail.com>
2023-10-25 12:30:20 -0700

    tweaks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -6,8 +6,12 @@ import { GOOGLE_CLIENT_SECRET, GOOGLE_CLIENT_ID, AUTH_SECRET, DISCORD_CLIENT_ID,
 export const handle = SvelteKitAuth(async (event) => {
     const authOptions = {
         providers: [Google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET }), Discord({ clientId: DISCORD_CLIENT_ID, clientSecret: DISCORD_CLIENT_SECRET})],
+        session: {
+            maxAge: 7 * 24 * 60 * 60,
+            updateAge: 1 * 24 * 60 * 60
+        },
+        trustHost: true,
         secret: AUTH_SECRET,
-        trustHost: true
     }
     return authOptions;
 })
