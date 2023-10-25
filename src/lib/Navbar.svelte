37c6a372db9597be37f80ccb5feb4e763183d40f
UnitedPuggs <edpoulsonv@hotmail.com>
2023-10-25 01:41:18 -0700

    fix: navbar style fix

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -9,7 +9,7 @@
         <img src="/assets/pancake.png" alt="smiling pancake" class="w-10 h-10 drop-shadow transition ease-linear hover:-translate-y-1">
     </a>
     {#if !$page.data.session?.user}
-        <a class="ml-auto mr-2 text-2xl font-bold hover:opacity-75" href="/login">login</a>
+        <a class="lg:ml-auto lg:mr-2 text-2xl font-bold hover:opacity-75" href="/login">login</a>
     {:else}
         <a href="my-links" class="lg:ml-auto lg:mr-2 text-2xl font-bold hover:opacity-75">my links</a>
         <a on:click={() => signOut()} class="lg:ml-2 text-2xl font-bold hover:opacity-75" href="/">log out</a>
