8353d4c831540fe6fd6a759babc76ff6cdb691f9
UnitedPuggs <edpoulsonv@hotmail.com>
2023-10-25 18:17:46 -0700

    navbar testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
     import "../app.css";
-    import { signIn } from "@auth/sveltekit/client";
     import Navbar from "$lib/Navbar.svelte";
 </script>
 
-<button on:click={() => signIn("discord")}>log in</button>
+<Navbar />
 <body class="bg-amber-200 p-2">
     <slot />
 </body>
