3d8610c4b4b3e338f187a8f63d93c742a6b95ffd
UnitedPuggs <edpoulsonv@hotmail.com>
2023-12-08 17:21:16 -0800

    fix: adjusted feed item widths for mobile devices

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -12,7 +12,7 @@
         {#each data.feed as feed}
             <a href="/{feed.stack_id}">
                 <div 
-                class="flex flex-col justify-center items-center border-2 border-black bg-white rounded-lg p-2 hover:opacity-80 w-40 lg:w-72 h-32 overflow-clip break-all">
+                class="flex flex-col justify-center items-center border-2 border-black bg-white rounded-lg p-2 hover:opacity-80 w-32 lg:w-72 h-32 overflow-clip break-all">
                     <span class="font-bold text-lg">{feed.stack_name}</span>
                     <span class="text-sm lg:text-base">https://shortstack.link/{feed.stack_id}</span>
                 </div>
